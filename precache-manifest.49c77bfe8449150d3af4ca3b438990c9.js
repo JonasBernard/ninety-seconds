d676cffb8d4764384994381ca80e8bcbe3fbb431
Jonas Bernard <contact@jonas-bernard.dev>
2025-01-15 19:30:26 +0000

    Updates

---
+++ precache-manifest.49c77bfe8449150d3af4ca3b438990c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fc468f54da9437b2ea2",
+    "url": "/ninety-seconds/static/js/runtime~main.4fc468f5.js"
+  },
+  {
+    "revision": "5625a3e11abfcef02f17",
+    "url": "/ninety-seconds/static/js/main.5625a3e1.chunk.js"
+  },
+  {
+    "revision": "8a8cd1dc674c7720e55f",
+    "url": "/ninety-seconds/static/js/2.8a8cd1dc.chunk.js"
+  },
+  {
+    "revision": "5625a3e11abfcef02f17",
+    "url": "/ninety-seconds/static/css/main.dc67e603.chunk.css"
+  },
+  {
+    "revision": "5abc93e3c2ae70fa9252d9a6c2310412",
+    "url": "/ninety-seconds/index.html"
+  }
+];
